a94b9d40017bed9be938a4f0f05901da330029dc
LaTienAnh <latienanh328@gmail.com>
2025-01-22 00:27:29 +0700

    test1

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class ListNode {
+     public int val;
+     public ListNode next;
+     public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+         this.next = next;
+     }
+  }
+ 
+public static class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        var l3 = new ListNode();
+        while ()
+        {
+            
+        }
+    }
+}
+
+public class Program()
+{
+    public static void Main(string[] args)
+    {
+        var l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        Solution.AddTwoNumbers(l1, l2);
+    }
+}
