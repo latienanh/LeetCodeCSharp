e826e2b33336216f9e341ef381b6d1b471e7cd07
LaTienAnh <latienanh328@gmail.com>
2025-01-22 23:28:13 +0700

    done

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleAppLeetCode;
+
+public static class Solution
+{
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
