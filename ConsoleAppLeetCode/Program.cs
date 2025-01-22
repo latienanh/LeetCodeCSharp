36751d245397f0bb599db47a96600771bf958885
LaTienAnh <latienanh328@gmail.com>
2025-01-23 01:22:45 +0700

    wrong

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -1,14 +1,61 @@
-﻿namespace ConsoleAppLeetCode;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppLeetCode;
 
 public static class Solution
 {
+    public static int LengthOfLongestSubstring(string s)
+    {
+        var subString = new List<string>();
+        var index = 0;
+        foreach (var charString in s)
+        {
+            if (subString.Count <= index)
+            {
+                subString.Add("");
+            }
 
+            if (subString[index] == "")
+                    subString[index]+=charString;
+            else
+            {
+                foreach (var charSubString in subString[index])
+                {
+                    if (charString == charSubString)
+                    {
+                        index++;
+                        if (subString.Count <= index)
+                        {
+                            subString.Add("");
+                        }
+                        if (subString[index] == "")
+                        break;
+                    }
+                }
+                subString[index] += charString;
+            }
+            
+        }
+        var maxLengthSubString = 0;
+        {
+            foreach (var a in subString)
+            {
+                if (a.Length > maxLengthSubString)
+                {
+                    maxLengthSubString = a.Length;
+                }
+            }
+        }
+        return maxLengthSubString;
+    }
 }
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        var testString = "abcabcbb";
+        var maxLengthSubString = Solution.LengthOfLongestSubstring(testString);
+        Console.WriteLine(maxLengthSubString);
     }
 }
