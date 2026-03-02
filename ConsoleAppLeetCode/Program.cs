fa3aed45a901a8591cec4fdc8fb526b6ecaeeb10
anhlt <latienanh328@gmail.com>
2026-03-02 18:16:31 +0700

    update code

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -2,101 +2,106 @@
 Console.WriteLine("Hello, World!");
 
 var a = new Solution();
-Console.WriteLine(a.IsValid("()"));
+ListNode list1 = new ListNode(2, null);
+ListNode list2 = new ListNode(1, null);
+var list3 = a.MergeTwoLists(list1, list2);
 
-// dùng loop
+Console.WriteLine("Done");
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 //public class Solution
 //{
-//    public bool IsValid(string s)
+//    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
 //    {
-//        var list = s.ToList();
-
-//        if (list.Count % 2 != 0)
+//        if (list1 == null)
 //        {
-//            return false;
+//            return list2;
 //        }
-//        var dict = new Dictionary<char, char>()
+//        if (list2 == null)
 //        {
-//            {'[', ']'},
-//            {'(', ')'},
-//            {'{', '}'},
-//            {'n', 'n'},
-//        };
-//        var listCheck = new char[]
-//        {
-//            ']', ')', '}'
-//        };
-//        for (int i = 0; i < list.Count; i++)
-//        {
-//            if (listCheck.Contains(list[i])) 
-//            {
-//                for (int j = i - 1; j >= 0; j--)
-//                {
-//                    if (dict.TryGetValue(list[j], out var value))
-//                    {
-//                        if (value == list[i])
-//                        {
-//                            list[i] = 'n';
-//                            list[j] = 'n';
-//                            break;
-//                        }
-//                        else if(value == 'n')
-//                        {
-//                            continue;
-//                        }
-//                        else
-//                        {
-//                            return false;
-//                        }
-//                    }
+//            return list1;
+//        }
 
-//                }
+//        ListNode listSort = null;
+//        var p1 = list1;
+//        var p2 = list2;
+//        while (p1 != null || p2 != null)
+//        {
+//            if (p1 == null)
+//            {
+//                AddLast(head: ref listSort, p2.val);
+//                p2 = p2.next;
+//            }
+//            else if (p2 == null)
+//            {
+//                AddLast(head: ref listSort, p1.val);
+//                p1 = p1.next;
+//            }
+//            else if (p1.val < p2.val)
+//            {
+//                AddLast(head: ref listSort, p1.val);
+//                p1 = p1.next;
+//            }
+//            else
+//            {
+//                AddLast(head: ref listSort, p2.val);
+//                p2 = p2.next;
 //            }
 //        }
+//        return listSort;
+//    }
+//    public void AddLast(ref ListNode head, int value)
+//    {
+//        var newNode = new ListNode(value);
 
-//        foreach (var l in list)
+//        if (head == null)
 //        {
-//            if (l != 'n')
-//            {
-//                return false;
-//            }
+//            head = newNode;
+//            return;
 //        }
-//        return true;
+
+//        var current = head;
+
+//        while (current.next != null)
+//        {
+//            current = current.next;
+//        }
+
+//        current.next = newNode;
 //    }
 //}
-
-//stack
+//v2
 public class Solution
 {
-    public bool IsValid(string s)
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
-        if (s.Length % 2 != 0)
+        
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (list1 != null && list2 != null)
         {
-            return false;
-        }
-        var stack = new Stack<char>();
-
-        var dict = new Dictionary<char, char>()
-        {
-            {']', '['},
-            {')', '('},
-            {'}', '{'},
-        };
-
-        foreach (var c in s)
-        {
-            if (dict.TryGetValue(c, out var value))
+            if (list1.val < list2.val)
             {
-                if (stack.Count == 0 || value != stack.Pop())
-                {
-                    return false;
-                }
+                tail.next = list1;
+                list1 = list1.next;
             }
             else
             {
-                stack.Push(c);
+                tail.next = list2;
+                list2 = list2.next;
             }
+            tail = tail.next;
         }
-        return stack.Count == 0;
+        tail.next = list1 ?? list2;
+        return dummy.next;
     }
 }
