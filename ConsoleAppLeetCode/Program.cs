99af64fda70df66eeeaf05519883917e29f66b7d
anhlt <latienanh328@gmail.com>
2026-03-04 17:53:32 +0700

    done code

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -1,23 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-int k = new Solution().RemoveDuplicates(nums);
+int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+int k = new Solution().RemoveElement(nums, 2);
 
 Console.WriteLine($"Result:{k}");
 public class Solution
 {
-    public int RemoveDuplicates(int[] nums)
+    public int RemoveElement(int[] nums, int val)
     {
-        int k = 1;
-        for (int i = 1; i < nums.Length; i++)
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (nums[i] != nums[k-1])
+            if (nums[i] != val)
             {
                 nums[k] = nums[i];
                 k++;
             }
+            
         }
+
         return k;
     }
 }
