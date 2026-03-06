15d98d51b28026d60674d28db72a0e4b92715c76
anhlt <latienanh328@gmail.com>
2026-03-06 17:51:39 +0700

    done code

--- ConsoleAppLeetCode/Program.cs
+++ ConsoleAppLeetCode/Program.cs
@@ -1,42 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int[] nums = [1, 3];
-int target = 2;
+string s = "   fly me   to   the moon  ";
 
-int k = new Solution().SearchInsert(nums, target);
+int k = new Solution().LengthOfLastWord(s);
 
 Console.WriteLine($"Result:{k}");
 public class Solution
 {
-    public int SearchInsert(int[] nums, int target)
+    public int LengthOfLastWord(string s)
     {
+        int count = 0;
 
-        int left = 0;
-        int right = nums.Length - 1;
-
-        if (nums[left] > target)
-            return 0;
-        if (nums[right] < target)
+        for (int i = s.Length - 1; i >= 0; i--)
         {
-            return right + 1;
-        }
+            if (s[i] == ' ' && count == 0)
+            {
+                continue;
+            }
+            if (s[i] == ' ' && count > 0)
+            {
+                break;
+            }
 
-        while (left <= right)
-        {
-            int mid = left + (right-left) / 2;
-            if (nums[mid] == target)
+            if (s[i] != ' ')
             {
-                return mid;
-            }
-            else if (nums[mid] < target)
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
+                count++;
             }
         }
-        return left;
+        return count;
     }
 }
